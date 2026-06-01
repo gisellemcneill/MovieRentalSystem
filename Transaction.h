24dcd5ab3e6f1f013fc2e8407015ee888201293c
Giselle McNeill <gisellemcneill@mac.mynetworksettings.com>
2026-05-31 20:31:04 -0700

    Implemented Transaction abstract base class

---
+++ Transaction.h
@@ -0,0 +1,60 @@
+// ------------------------------------------------ Transaction.h --------------------------------------------------------
+
+// Giselle McNeill, Nash Kumia CSS343 - 11AM T/TH with Wooyoung Kim
+
+// Creation Date: 5/31/26
+
+// Date of Last Modification: 5/31/26
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Purpose:
+// Defines the Transaction class, which serves as the abstract base class for all transaction types in the
+// movie rental store inventory system. Transaction will declare shared methods and attributes for derived
+// transaction classes, including type, movie, customer
+
+// Transaction object pointers will be stored in the customer class for the transaction history
+// Transactions will also be used to execute commands by the Company class on the inventory system.
+
+// The class uses a virtual destructor to ensure proper memory cleanup when deleting derived
+// class objects through a base class pointer. Derived classes will include BorrowTransaction and
+// ReturnTransaction must implement the pure virtual functions execute() and display()
+// --------------------------------------------------------------------------------------------------------------------
+
+
+#ifndef PROGRAM4_TRANSACTION_H
+#define PROGRAM4_TRANSACTION_H
+
+#include <iostream>
+#include "Movie.h"
+
+using namespace std;
+
+//Company is not yet defined, and is needed as a parameter for execute()
+class Company;
+
+class Transaction {
+public:
+    //constructor
+    Transaction(char type, Movie* movie, int customerID);
+    virtual ~Transaction(); //virtual destructor
+
+    //getters
+    char getType() const;
+    Movie* getMovie() const;
+    int getCustomerID() const;
+
+    //pure virtual functions
+    virtual bool execute(Company& company) = 0;
+    virtual void display(ostream& out) const = 0;
+
+    //To be used with display
+    friend ostream& operator<<(ostream& out, const Transaction& t);
+
+protected:
+    char type;
+    Movie* movie;
+    int customerID;
+};
+
+#endif //PROGRAM4_TRANSACTION_H
